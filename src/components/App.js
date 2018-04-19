da389dde449b83bdf921a36d9e65d8d53d263c5e
Tianjian Wang <wangtjwork@gmail.com>
2018-04-18 21:54:24 -0500

    refactor: Move all components into components/

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react'
+import { connect } from 'react-redux'
+import {
+  handleInitialData
+} from '../actions/shared'
+import ConnectedTodos from './Todos'
+import ConnectedGoals from './Goals'
 
-class App extends Component {
+class App extends React.Component {
+  componentDidMount() {
+    const {dispatch} = this.props;
+
+    dispatch(handleInitialData());
+  }
+
   render() {
+
+    if (this.props.loading) {
+      return <h3>Now loading...</h3>
+    }
+
     return (
       <div>
-        Hello World
+        <ConnectedTodos/>
+        <ConnectedGoals/>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default connect((state) => ({
+  loading: state.loading
+}))(App);
